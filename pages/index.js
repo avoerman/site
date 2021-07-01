165df08d6c4aa2d93292ffff00b99c78b4e68dd1
Alex Voerman <avoerman89@gmail.com>
2021-06-30 20:09:36 -0400

    footer

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
+          <a href={"https://github.com/avoerman/site.git"}>https://github.com/avoerman/site.git</a>
       </footer>
     </div>
   )
